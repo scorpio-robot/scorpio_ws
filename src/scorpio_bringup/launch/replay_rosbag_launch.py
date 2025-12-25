8f1f6791379537d8ba04e817d7d2bbb87b41bece
LihanChen2004 <757003373@qq.com>
2025-12-25 11:16:47 +0800

    🎉 feat: Add launch file to enable replay rosbags

---
+++ src/scorpio_bringup/launch/replay_rosbag_launch.py
@@ -0,0 +1,94 @@
+# Copyright 2025 Lihan Chen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    # Get the launch directory
+    bringup_dir = get_package_share_directory("scorpio_bringup")
+    scorpio_description = get_package_share_directory("scorpio_description")
+
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration("namespace")
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        "namespace", default_value="", description="Top-level namespace"
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=os.path.join(
+            bringup_dir, "params", "reality", "scorpio_reality.yaml"
+        ),
+        description="Full path to the ROS2 parameters file to use for all launched driver nodes",
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="True",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart",
+        default_value="True",
+        description="Automatically startup the nav2 stack",
+    )
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        "use_respawn",
+        default_value="False",
+        description="Whether to respawn if a node crashes. Applied when composition is disabled.",
+    )
+
+    declare_log_level_cmd = DeclareLaunchArgument(
+        "log_level", default_value="error", description="log level"
+    )
+
+    bringup_robot_description_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                scorpio_description, "launch", "robot_state_publisher_launch.py"
+            )
+        ),
+        launch_arguments={
+            "namespace": namespace,
+            "use_sim_time": "True",
+            "robot_name": "scorpio",
+            "visualize_robot_desc": "False",
+        }.items(),
+    )
+
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_log_level_cmd)
+
+    # Running Map Saver Server
+    ld.add_action(bringup_robot_description_cmd)
+
+    return ld
