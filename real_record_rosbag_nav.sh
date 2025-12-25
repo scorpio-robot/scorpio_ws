6a30d8f4ca7033f33e7a77d896bb60c4397eadc6
LihanChen2004 <757003373@qq.com>
2025-12-25 22:20:45 +0800

    remove scripts in root directory

--- real_record_rosbag_nav.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# Record nav rosbag in real world
-source install/setup.zsh
-
-ros2 bag record -o real_car_mid360_$(date +%Y%m%d_%H%M%S) \
-  /joint_states \
-  /livox/imu \
-  /livox/lidar
--- scripts/record_mid360.sh
+++ scripts/record_mid360.sh
@@ -3,6 +3,7 @@
 # Record mid360 rosbag
 source install/setup.zsh
 
-ros2 bag record -o mid360_$(date +%Y%m%d_%H%M%S) \
+ros2 bag record -o real_car_$(date +%Y%m%d_%H%M%S) \
   livox/lidar \
-  livox/imu
+  livox/imu \
+  joint_states
