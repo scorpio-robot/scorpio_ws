5ff16300215e41b0cd829809b6997230acb401f0
LihanChen2004 <757003373@qq.com>
2026-02-20 16:54:08 +0800

    ✨ feat: Update build and test scripts to use dynamic package list

--- build.sh
+++ build.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 set -e
 
-# Set the default build type
-BUILD_TYPE=RelWithDebInfo
+# Default build type
+BUILD_TYPE=${BUILD_TYPE:-RelWithDebInfo}
+# Default packages if no argument
+DEFAULT_PACKAGES="loam_interface"
+
+# Determine package filter
+# Usage:
+#   ./build.sh          -> build DEFAULT_PACKAGES (and their deps)
+#   ./build.sh all      -> build entire workspace
+#   ./build.sh pkg1 ... -> build specified packages (and their deps)
+if [ "$#" -eq 0 ]; then
+  PACKAGES_ARGS="--packages-up-to $DEFAULT_PACKAGES"
+elif [ "$1" = "all" ]; then
+  PACKAGES_ARGS=""
+else
+  PACKAGES_ARGS="--packages-up-to $@"
+fi
 
 colcon build \
   --merge-install \
   --symlink-install \
   --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" \
   --parallel-workers 10 \
-  --packages-up-to loam_interface
+  $PACKAGES_ARGS
--- test.sh
+++ test.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 set -e
 
+# Default packages if no argument
+DEFAULT_PACKAGES="loam_interface"
+
+# Determine package filter
+# Usage:
+#   ./test.sh           -> test DEFAULT_PACKAGES
+#   ./test.sh all       -> test entire workspace
+#   ./test.sh pkg1 ...  -> test specified packages
+if [ "$#" -eq 0 ]; then
+  PACKAGES_ARGS="--packages-select $DEFAULT_PACKAGES"
+elif [ "$1" = "all" ]; then
+  PACKAGES_ARGS=""
+else
+  PACKAGES_ARGS="--packages-select $@"
+fi
+
 if [ -f install/setup.bash ]; then source install/setup.bash; fi
+
 colcon test \
   --merge-install \
   --parallel-workers 10 \
-  --packages-select loam_interface
+  $PACKAGES_ARGS
 colcon test-result --all --verbose
